632b526dc6a88410f54f75107cff8915af03d4ef
Vinicius Santos <vinisantoosk@gmail.com>
2024-10-21 23:33:55 -0300

    feat: parametrizando os filtros

--- src/components/FiltroCard/styles.ts
+++ src/components/FiltroCard/styles.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
 import { Props } from '.'
 
-export const Card = styled.div<Props>`
+type PropsSemLegendaESemContador = Omit<Props, 'contador' | 'legenda'>
+
+export const Card = styled.div<PropsSemLegendaESemContador>`
   padding: 8px;
   border: 1px solid ${(props) => (props.ativo ? '#1e90ff' : '#a1a1a1')};
   background-color: ${(props) => (props.ativo ? '#fff' : '#fcfcfc')}#fcfcfc;
--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -7,12 +7,12 @@ const BarraLateral = () => (
     <div>
       <S.Campo type="text" placeholder="buscar" />
       <S.Filtros>
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard ativo />
+        <FiltroCard contador={3} legenda="pendentes" />
+        <FiltroCard contador={4} legenda="concluídas" />
+        <FiltroCard contador={5} legenda="urgentes" />
+        <FiltroCard contador={2} legenda="importantes" />
+        <FiltroCard contador={4} legenda="normal" />
+        <FiltroCard contador={10} legenda="todas" ativo />
       </S.Filtros>
     </div>
   </S.Aside>
