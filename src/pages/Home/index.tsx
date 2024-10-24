d3297396d6db1e280dc1f3ced15f76f72feda7ea
Vinicius Santos <vinisantoosk@gmail.com>
2024-10-24 11:41:36 -0300

    config: aplicando router dom

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import BarraLateral from '../../containers/BarraLateral'
+import ListaDeTarefas from '../../containers/ListaDeTarefas'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListaDeTarefas />
+  </>
+)
+export default Home
